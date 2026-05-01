dc819e330b52bf47427a2faca5ef93bbde3d5603
Philipp Grosswiler <philipp.grosswiler@post.ch>
2026-05-01 17:56:23 +0200

    build: constrain vulnerable plugin dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,26 @@ plugins {
     alias(libs.plugins.binary.compatibility.validator)
 }
 
+buildscript {
+    dependencies {
+        constraints {
+            classpath("org.bouncycastle:bcpg-jdk18on:${libs.versions.bouncycastle.get()}")
+            classpath("org.bouncycastle:bcpkix-jdk18on:${libs.versions.bouncycastle.get()}")
+            classpath("org.bouncycastle:bcprov-jdk18on:${libs.versions.bouncycastle.get()}")
+            classpath("org.bouncycastle:bcutil-jdk18on:${libs.versions.bouncycastle.get()}")
+            classpath("org.bitbucket.b_c:jose4j:${libs.versions.jose4j.get()}")
+            classpath("org.jdom:jdom2:${libs.versions.jdom2.get()}")
+            classpath("org.apache.commons:commons-lang3:${libs.versions.commonsLang3.get()}")
+            classpath("org.apache.httpcomponents:httpclient:${libs.versions.httpclient.get()}")
+            classpath("com.fasterxml.jackson.core:jackson-annotations:${libs.versions.jackson.get()}")
+            classpath("com.fasterxml.jackson.core:jackson-core:${libs.versions.jackson.get()}")
+            classpath("com.fasterxml.jackson.core:jackson-databind:${libs.versions.jackson.get()}")
+            classpath("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:${libs.versions.jackson.get()}")
+            classpath("com.fasterxml.jackson.module:jackson-module-kotlin:${libs.versions.jackson.get()}")
+        }
+    }
+}
+
 allprojects {
     group = "ch.trancee.meshlink"
     version = "0.1.0-SNAPSHOT"
